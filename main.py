42e1750a1af8df74a0295d9236dbe2d053194a42
Plavak Das <plavakdas@gmail.com>
2024-09-21 17:21:58 +0530

    removed unused import

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
